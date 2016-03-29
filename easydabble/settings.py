9aa209475bc80415a913a63249ce580580a53334
Jared <jaredrussell93@gmail.com>
2016-03-29 12:03:27 -0400

    more settings

--- easydabble/settings.py
+++ easydabble/settings.py
@@ -110,6 +110,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
