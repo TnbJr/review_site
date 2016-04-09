802e81ccb9e5c2bc0087bd198062db002aec7dba
Jared <jaredrussell93@gmail.com>
2016-04-09 14:01:40 -0400

    test

--- easydabble/settings.py
+++ easydabble/settings.py
@@ -1,5 +1,6 @@
 import os 
 # from .secret import SECRET_KEY
+#/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
